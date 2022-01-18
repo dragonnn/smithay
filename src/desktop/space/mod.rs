ed6d1d4ba6cf33d07839d68b3b013bd1b3d5f5ee
dragonn <dragonn@op.pl>
2022-01-18 22:03:24 +0100

    Revert "(draft) implement bottom_custom_elements"
    This reverts commit 1581f7593b173f83b49df57aa4b6bc85c9770b51.

--- src/desktop/space/mod.rs
+++ src/desktop/space/mod.rs
@@ -462,8 +462,7 @@ impl Space {
         output: &Output,
         age: usize,
         clear_color: [f32; 4],
-        bottom_custom_elements: &[DynamicRenderElements<R>],
-        top_custom_elements: &[DynamicRenderElements<R>],
+        custom_elements: &[DynamicRenderElements<R>],
     ) -> Result<Option<Vec<Rectangle<i32, Logical>>>, RenderError<R>>
     where
         R: Renderer + ImportAll + 'static,
@@ -510,11 +509,10 @@ impl Space {
                     .windows
                     .iter()
                     .map(|w| w as &SpaceElem<R>)
-                    .chain(bottom_custom_elements.iter().map(|p| p as &SpaceElem<R>))
                     .chain(window_popups.iter().map(|p| p as &SpaceElem<R>))
                     .chain(layer_map.layers().map(|l| l as &SpaceElem<R>))
                     .chain(layer_popups.iter().map(|p| p as &SpaceElem<R>))
-                    .chain(top_custom_elements.iter().map(|c| c as &SpaceElem<R>))
+                    .chain(custom_elements.iter().map(|c| c as &SpaceElem<R>))
                     .any(|e| ToplevelId::from(e) == *id)
                 {
                     Some(*geo)
@@ -533,11 +531,10 @@ impl Space {
             .windows
             .iter()
             .map(|w| w as &SpaceElem<R>)
-            .chain(bottom_custom_elements.iter().map(|c| c as &SpaceElem<R>))
             .chain(window_popups.iter().map(|p| p as &SpaceElem<R>))
             .chain(layer_map.layers().map(|l| l as &SpaceElem<R>))
             .chain(layer_popups.iter().map(|p| p as &SpaceElem<R>))
-            .chain(top_custom_elements.iter().map(|c| c as &SpaceElem<R>))
+            .chain(custom_elements.iter().map(|c| c as &SpaceElem<R>))
         {
             let geo = element.geometry(self.id);
             let old_geo = state.last_state.get(&ToplevelId::from(element)).cloned();
@@ -620,7 +617,6 @@ impl Space {
                     .layers_on(WlrLayer::Background)
                     .chain(layer_map.layers_on(WlrLayer::Bottom))
                     .map(|l| l as &SpaceElem<R>)
-                    .chain(bottom_custom_elements.iter().map(|c| c as &SpaceElem<R>))
                     .chain(self.windows.iter().map(|w| w as &SpaceElem<R>))
                     .chain(
                         layer_map
@@ -628,7 +624,7 @@ impl Space {
                             .chain(layer_map.layers_on(WlrLayer::Overlay))
                             .map(|l| l as &SpaceElem<R>),
                     )
-                    .chain(top_custom_elements.iter().map(|c| c as &SpaceElem<R>))
+                    .chain(custom_elements.iter().map(|c| c as &SpaceElem<R>))
                 {
                     let geo = element.geometry(self.id);
                     if damage.iter().any(|d| d.overlaps(geo)) {
@@ -674,11 +670,10 @@ impl Space {
             .windows
             .iter()
             .map(|w| w as &SpaceElem<R>)
-            .chain(bottom_custom_elements.iter().map(|c| c as &SpaceElem<R>))
             .chain(window_popups.iter().map(|p| p as &SpaceElem<R>))
             .chain(layer_map.layers().map(|l| l as &SpaceElem<R>))
             .chain(layer_popups.iter().map(|p| p as &SpaceElem<R>))
-            .chain(top_custom_elements.iter().map(|c| c as &SpaceElem<R>))
+            .chain(custom_elements.iter().map(|c| c as &SpaceElem<R>))
             .map(|elem| {
                 let geo = elem.geometry(self.id);
                 (ToplevelId::from(elem), geo)
